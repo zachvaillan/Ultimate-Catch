aabffa8be0fa372f286e436b615a24f787ec3139
Zach Vaillancourt <zsvail98@live.com>
2020-11-11 15:36:59 -0800

    fix posts routes

--- frontend/src/components/posts/posts_index.js
+++ frontend/src/components/posts/posts_index.js
@@ -3,7 +3,7 @@ import { withRouter } from 'react-router-dom';
 import PostIndexItem from './post_index_item';
 // import Map from '../map/map';
 // import './post_index.css';
-import post_index_item from './post_index_item';
+// import PostIndexItem from './post_index_item';
 // import '../map/map.css';
 
 class PostIndex extends React.Component {
@@ -86,14 +86,14 @@ class PostIndex extends React.Component {
           <div className="main-content">
             <p>{console.log(this.state)}</p>
             <div className="map-container">
-            <div className="sticky-map-container"> <Map 
+            {/* <div className="sticky-map-container"> <Map 
             fetchPost={this.props.fetchPost} 
             fetchWeather={this.props.fetchWeather} 
             regions={this.props.regions} 
             posts={this.state.posts} 
             fetchPosts={this.props.fetchPost} 
             handleRegionChange={this.handleRegionChange} 
-            fetchRegions={this.props.fetchRegions} /> </div>
+            fetchRegions={this.props.fetchRegions} /> </div> */}
             </div>
             <div className="posts-idx-main-container">
               <div className="posts-idx-main" >
