fbb72bc76636b73e1fe908dc737da47806784382
Zach Vaillancourt <zsvail98@live.com>
2020-11-10 17:12:27 -0800

    add rest of util and reducers

---
+++ frontend/src/reducers/entities_reducer.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+// import weather from './weather_reducer';
+import posts from './posts_reducer';
+// import regions from './regions_reducer';
+
+export default combineReducers({
+    // weather,
+    posts
+    // regions
+});
