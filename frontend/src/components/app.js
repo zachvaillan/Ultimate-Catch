58342bd193f89970b620ba8ff1df5d3658b536de
Zach Vaillancourt <zsvail98@live.com>
2020-11-11 15:18:24 -0800

    comment unused imports and installed necessary things

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -6,12 +6,12 @@ import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import DemoFormContainer from './session/demo_form_container';
-import MapContainer from './map/map_container';
+// import MapContainer from './map/map_container';
 import PostsIndexContainer from './posts/posts_index_container';
 import PostFormContainer from './posts/post_form_container';
 import SplashPage from './splash/splash';
-import FooterContainer from './footer/footer_container'
-import Profile from './profile/profile'
+// import FooterContainer from './footer/footer_container'
+// import Profile from './profile/profile'
 
 const App = () => (
   <div>
@@ -23,7 +23,7 @@ const App = () => (
         <ProtectedRoute exact path="/main" component={MainPage} />
         <ProtectedRoute exact path="/posts" component={PostsIndexContainer} />
         <ProtectedRoute exact path="/posts/new" component={PostFormContainer} />
-        <ProtectedRoute exact path="/profile" component={Profile} />
+        {/* <ProtectedRoute exact path="/profile" component={Profile} /> */}
         <AuthRoute exact path="/login" component={LoginFormContainer} />
         <AuthRoute exact path="/signup" component={SignupFormContainer} />
         <AuthRoute exact path='/demo' component={DemoFormContainer} />
