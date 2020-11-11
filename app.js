5a59adf9dd67a00ef0fd60cfab80640d9b0e2a09
Zach Vaillancourt <zsvail98@live.com>
2020-11-10 16:29:52 -0800

    merge over this

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
-const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+const port = process.env.PORT || 5000;
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
+
+const users = require("./routes/api/users");
+// const posts = require("./routes/api/posts");
+// const files = require("./routes/api/files");
+
+// const passport = require('passport');
+const bodyParser = require('body-parser');
+
+mongoose
+.connect(db, { useNewUrlParser: true })
+.then(() => console.log("Connected to MongoDB successfully"))
+.catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello Wdddorld"));
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-const port = process.env.PORT || 5000;
-
-
-app.get("/", (req, res) => res.send("Hello World"));
 app.listen(port, () => console.log(`Server is running on port ${port}`));
