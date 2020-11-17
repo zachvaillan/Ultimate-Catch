ef4ca0c1ba96c7b3e0eb1b5c0c8f28e0ebaeb1c0
Zach Vaillancourt <zsvail98@live.com>
2020-11-16 16:44:28 -0800

    add users show page for current user

--- frontend/src/components/posts/user_posts_container.js
+++ frontend/src/components/posts/user_posts_container.js
@@ -1,17 +1,23 @@
 import { fetchUserPosts } from '../../actions/post_actions';
 import { connect } from 'react-redux';
 import UserPosts from './user_posts';
+import { heartPost, fetchPost, unheartPost, leaveComment } from '../../actions/post_actions';
 
 const mapStateToProps = state => {
     return {
         user: state.session.user,
+        userId: state.session.user.id,
         userPosts: Object.values(state.entities.posts.user)
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-      fetchUserPosts: userId => dispatch(fetchUserPosts(userId))
+        fetchUserPosts: userId => dispatch(fetchUserPosts(userId)),
+        heartPost: post => dispatch(heartPost(post)),
+        unheartPost: post => dispatch(unheartPost(post)),
+        leaveComment: (postId, commentData) => dispatch(leaveComment(postId, commentData)),
+        fetchPost: (id) => dispatch(fetchPost(id))
     };
 };
 
